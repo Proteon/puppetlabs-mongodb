f014ecfa918506ea01b0f88fa4c37bdf63629e17
UltimatePancake <pier.gaetani@gmail.com>
2016-05-18 04:21:55 -0600

    PG: backup command correction

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -5,7 +5,7 @@ class mongodb::backup (
 ) inherits mongodb::params {
   # Backup cron job
   cron { 'MongoDB backup':
-    command  => 'mongodump --username $admin_username --password $admin_password --out /opt/backups/mongo.`date +%Y%m%d` --quiet && wait; cd /opt/backups && wait; tar czf /opt/backups/mongo.`date +%Y%m%d`.tar.gz mongo.`date +%Y%m%d` && wait; rm -Rf mongo.`date +%Y%m%d` && wait;'
+    command  => 'mongodump --username $admin_username --password $admin_password --out /opt/backups/mongo.`date +%Y%m%d` --quiet && wait; cd /opt/backups && wait; tar czf /opt/backups/mongo.`date +%Y%m%d`.tar.gz mongo.`date +%Y%m%d` && wait; rm -Rf mongo.`date +%Y%m%d` && wait;',
     user     => 'root',
     hour     => 23,
     minute   => 0,
