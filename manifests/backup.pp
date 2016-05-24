4a08e84673352e71751ae6933b92163df4445ba6
UltimatePancake <pier.gaetani@gmail.com>
2016-05-24 15:00:47 -0600

    PG missing parameter

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -3,7 +3,7 @@ class mongodb::backup (
   $backupuser,
   $backuppassword,
   $backupdir,
-  $backupdirmode = '0700'
+  $backupdirmode = '0700',
   $backupdirowner = 'root',
   $backupdirgroup = 'root',
   $backuprotate = 30,
