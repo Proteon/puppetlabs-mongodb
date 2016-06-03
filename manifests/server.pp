57c4c75b37344d25a1d5a1dccc4c385ab9cf5b31
UltimatePancake <pier.gaetani@gmail.com>
2016-06-03 08:58:41 -0600

    Revert "PG corrected backup directory"
    This reverts commit ab190be11ad17b6a385637d565e32edd053e2a11.

--- manifests/server.pp
+++ manifests/server.pp
@@ -182,6 +182,6 @@ class mongodb::server (
   class { 'mongodb::backup':
     backupuser     => $admin_username,
     backuppassword => $admin_password,
-    backupdir      => '/opt/backups/mongo'
+    backupdir      => '/opt/exclude/mongo'
   }
 }
